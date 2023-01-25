2fa3e5ba73af0c7f890d9d5adfbfac3a19d2ecd8
PaulNowak36 <paul.nowak@algosup.com>
2023-01-25 14:53:24 +0100

    tested vector3 coordinates

--- Cplusplus/main.cpp
+++ Cplusplus/main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <stdio.h>
-//#include "Vector2.cpp"
-#include "Vector2C.cpp"
+#include "Vector3/Vector3.cpp"
 
 // this file is used for testing. The vector library is located in vector.h.
 
@@ -11,12 +10,16 @@ int main() {
     // C++ CODE
 
     Vector3 p3(0, 0, 4);
+    printf("P3's coordinates are X:%f, Y:%f and Z:%f.\n", p3.x, p3.y, p3.z);
+
     Vector3 p4(0, 4, 0);
+    printf("P4's coordinates are X:%f, Y:%f and Z:%f.\n", p4.x, p4.y, p4.z);
+
     Vector3 mp = p3.midpoint(p4);
-    printf("The midpoint between points p1 and p2 is (%f, %f, %f).\n\n", mp.x, mp.y, mp.z);
+    printf("The midpoint between points p1 and p2 is (%f, %f, %f).\n", mp.x, mp.y, mp.z);
     // add your tests above this line.
-    printf("Testing finished, press enter to exit.");
-    cin.get();
+    
+
     return 0;
     
 }
