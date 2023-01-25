ce46f94eeb8d3b23e25934dcf235b1c116d1ea5c
PaulNowak36 <paul.nowak@algosup.com>
2023-01-25 15:04:42 +0100

    Vector3C created + coordinates C struct done

---
+++ Cplusplus/Vector3/Vector3C.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string>
+#include <math.h>
+
+extern "C" struct Vector3 {
+    double x;
+    double y;
+    double z;
+};
--- Cplusplus/main.cpp
+++ Cplusplus/main.cpp
@@ -1,23 +1,39 @@
 #include <iostream>
 #include <stdio.h>
-#include "Vector3/Vector3.cpp"
+#include "Vector3/Vector3C.cpp"
 
 // this file is used for testing. The vector library is located in vector.h.
 
 using namespace std;
 
 int main() {
-    // C++ CODE
+    // C CODE
 
-    Vector3 p3(0, 0, 4);
+    struct Vector3 p3;
+    struct Vector3 p4; 
+
+    p3.x = 0;
+    p3.y = 0;
+    p3.z = 4;
+
+    p4.x = 0;
+    p4.y = 4;
+    p4.z = 0;
+
     printf("P3's coordinates are X:%f, Y:%f and Z:%f.\n", p3.x, p3.y, p3.z);
 
-    Vector3 p4(0, 4, 0);
     printf("P4's coordinates are X:%f, Y:%f and Z:%f.\n", p4.x, p4.y, p4.z);
 
-    Vector3 mp = p3.midpoint(p4);
-    printf("The midpoint between points p1 and p2 is (%f, %f, %f).\n", mp.x, mp.y, mp.z);
-    // add your tests above this line.
+    // C++ CODE
+
+    // Vector3 p3(0, 0, 4);
+    // printf("P3's coordinates are X:%f, Y:%f and Z:%f.\n", p3.x, p3.y, p3.z);
+
+    // Vector3 p4(0, 4, 0);
+    // printf("P4's coordinates are X:%f, Y:%f and Z:%f.\n", p4.x, p4.y, p4.z);
+
+    // Vector3 mp = p3.midpoint(p4);
+    // printf("The midpoint between points p1 and p2 is (%f, %f, %f).\n", mp.x, mp.y, mp.z);
     
 
     return 0;
