85fc32ef384ba075e4529e5a9a80f333df952cf3
PaulNowak36 <paul.nowak@algosup.com>
2023-01-30 09:01:04 +0100

    small update

--- Cplusplus/Vector3/Vector3C.cpp
+++ Cplusplus/Vector3/Vector3C.cpp
@@ -10,6 +10,18 @@ extern "C" struct Vector3 {
     double z;
 };
 
+extern "C" double distanceTo(Vector2 pos, Vector2 end)
+{
+        return sqrt((pos.y - end.y) * (pos.y - end.y) + (pos.x - end.x) * (pos.x - end.x));
+}
+
+extern "C" Vector2 vectorMovement(Vector2 mov, double plusx, double plusy) {
+    mov.x += plusx;
+    mov.y += plusy;
+    return mov;
+}
+
+
 extern "C" Vector3 midpoint(Vector3 fst, Vector3 scd) {
         double mx = (fst.x + scd.x) / 2;
         double my = (fst.y + scd.y) / 2;
@@ -21,3 +33,7 @@ extern "C" Vector3 midpoint(Vector3 fst, Vector3 scd) {
 
         return mid;
 }
+
+extern "C" double percentDistance(Vector2 pos, Vector2 end, double percentOfDistance = 100) {
+        return distanceTo(pos, end) / (100 / percentOfDistance);
+    }
