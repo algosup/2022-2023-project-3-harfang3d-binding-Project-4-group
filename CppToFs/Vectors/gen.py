98ffb6e7b5e80fff5460acd597613109d5e2a0bd
AlexisLasselin <114481578+AlexisLasselin@users.noreply.github.com>
2023-02-09 16:34:04 +0100

    Windows version created

--- CppToFs/Vectors/gen.py
+++ CppToFs/Vectors/gen.py
@@ -17,23 +17,23 @@ else:
         "The file have not been found, please verify the location of your Program.fs")
 
 
-if os.path.exists(args.path+"compiledVector"):
-    os.remove(args.path+"compiledVector")
+if os.path.exists(args.path+"compiledVectors"):
+    os.remove(args.path+"compiledVectors")
 
-if os.path.exists(args.path+"compiledVector.exe"):
-    os.remove(args.path+"compiledVector.exe")
+if os.path.exists(args.path+"compiledVectors.exe"):
+    os.remove(args.path+"compiledVectors.exe")
 
 
 # compiling the related C++ file
 def compile_cpp(file_path):
     if platform == "darwin":  # macOs
-        subprocess.run(["g++", "-o", "compiledVector", file_path])
+        subprocess.run(["g++", "-o", "compiledVectors", file_path])
     elif platform == "win32":  # Windows
         subprocess.run(
-            ["g++", "-shared", "-o", "compiledVector.exe", "-m64", file_path])
+            ["g++", "-shared", "-o", "compiledVectors.exe", "-m64", file_path])
 
 
-compile_cpp("../../CppToFs/Vectors/Vector2.cpp")
+compile_cpp("../../CppToFs/Vectors/Vectors.cpp")
 
 if os.path.exists(args.path+"FSHeader2D.fs"):
     os.remove(args.path+"FSHeader2D.fs")
@@ -47,50 +47,50 @@ else:
 if platform == "darwin":
     Header2D = open(args.path+"FSHeader2D.fs", "w")
     imports2D = ["module vector2\n",
-                 'open System.Runtime.InteropServices\n',
+                'open System.Runtime.InteropServices\n',
 
-                 '[<StructLayout(LayoutKind.Sequential)>]\n',
-                 'type Vector2 = val mutable X: double; val mutable Y: double new(x, y) = { X = x; Y = y}\n',
+                '[<StructLayout(LayoutKind.Sequential)>]\n',
+                'type Vector2 = val mutable X: double; val mutable Y: double new(x, y) = { X = x; Y = y}\n',
 
-                 '[<DllImport("compiledVector")>]\n',
-                 'extern Vector2 CreateVector2D(double x, double y)\n',
+                '[<DllImport("compiledVectors")>]\n',
+                'extern Vector2 CreateVector2D(double x, double y)\n',
 
-                 '[<DllImport("compiledVector")>]\n',
-                 'extern double distanceTo2D(Vector2 v,Vector2 v2)\n',
+                '[<DllImport("compiledVectors")>]\n',
+                'extern double distanceTo2D(Vector2 v,Vector2 v2)\n',
 
-                 '[<DllImport("compiledVector")>]\n',
-                 'extern void vectorMovement2D(Vector2 v,double plusx, double plusy)\n',
+                '[<DllImport("compiledVectors")>]\n',
+                'extern void vectorMovement2D(Vector2 v,double plusx, double plusy)\n',
 
-                 '[<DllImport("compiledVector")>]\n',
-                 'extern Vector2 midpoint2D(Vector2 v,Vector2 v2)\n',
+                '[<DllImport("compiledVectors")>]\n',
+                'extern Vector2 midpoint2D(Vector2 v,Vector2 v2)\n',
 
-                 '[<DllImport("compiledVector")>]\n',
-                 'extern double percentDistance2D(Vector2 pos1, Vector2 pos2, double percent)\n']
+                '[<DllImport("compiledVectors")>]\n',
+                'extern double percentDistance2D(Vector2 pos1, Vector2 pos2, double percent)\n']
     for eachimports in imports2D:
         Header2D.write(eachimports)
         Header2D.flush()
 
     Header3D = open(args.path+"FSHeader3D.fs", "w")
     imports3D = ["module vector3\n",
-                 'open System.Runtime.InteropServices\n',
+                'open System.Runtime.InteropServices\n',
 
-                 '[<StructLayout(LayoutKind.Sequential)>]\n',
-                 'type Vector3 = val mutable X: double; val mutable Y:double; val mutable Z: double new(x, y, z) = { X = x; Y = y; Z=z }\n',
+                '[<StructLayout(LayoutKind.Sequential)>]\n',
+                'type Vector3 = val mutable X: double; val mutable Y:double; val mutable Z: double new(x, y, z) = { X = x; Y = y; Z=z }\n',
 
-                 '[<DllImport("compiledVector")>]\n',
-                 'extern Vector3 CreateVector3D(double x, double y, double z)\n',
+                '[<DllImport("compiledVectors")>]\n',
+                'extern Vector3 CreateVector3D(double x, double y, double z)\n',
 
-                 '[<DllImport("compiledVector")>]\n',
-                 'extern double distanceTo3D(Vector3 v, Vector3 v2)\n',
+                '[<DllImport("compiledVectors")>]\n',
+                'extern double distanceTo3D(Vector3 v, Vector3 v2)\n',
 
-                 '[<DllImport("compiledVector")>]\n',
-                 'extern void vectorMovement3D(Vector3 v, double plusx, double plusy, double plusz)\n',
+                '[<DllImport("compiledVectors")>]\n',
+                'extern void vectorMovement3D(Vector3 v, double plusx, double plusy, double plusz)\n',
 
-                 '[<DllImport("compiledVector")>]\n',
-                 'extern Vector3 midpoint3D(Vector3 v, Vector3 v2)\n',
+                '[<DllImport("compiledVectors")>]\n',
+                'extern Vector3 midpoint3D(Vector3 v, Vector3 v2)\n',
 
-                 '[<DllImport("compiledVector")>]\n',
-                 'extern double percentDistance3D(Vector3 pos1, Vector3 pos2, double percent)\n']
+                '[<DllImport("compiledVectors")>]\n',
+                'extern double percentDistance3D(Vector3 pos1, Vector3 pos2, double percent)\n']
     for eachimports in imports3D:
         Header3D.write(eachimports)
     Header3D.flush()
@@ -101,52 +101,49 @@ elif platform == "win32":
     else:
         Header2D = open(args.path+"FSHeader2D.fs", "w")
         imports2D = ["module vector2\n",
-                     'open System.Runtime.InteropServices\n',
+                'open System.Runtime.InteropServices\n',
 
-                     "[<StructLayout(LayoutKind.Sequential)>]\n",
-                     "type Vector2 = val mutable X: double; val mutable Y: double new(x, y) = { X = x; Y = y}\n",
-                     'printfn("test 1 passed")\n',
+                '[<StructLayout(LayoutKind.Sequential)>]\n',
+                'type Vector2 = val mutable X: double; val mutable Y: double new(x, y) = { X = x; Y = y}\n',
 
-                     '[<DllImport("compiledVector.exe", CallingConvention = CallingConvention.StdCall)>]\n',
-                     "extern Vector2 CreateVector2(double x, double y)\n",
+                '[<DllImport("compiledVectors.exe")>]\n',
+                'extern Vector2 CreateVector2D(double x, double y)\n',
 
-                     '[<DllImport("compiledVector.exe", CallingConvention = CallingConvention.StdCall)>]\n',
-                     "extern double distanceTo(Vector2 v,Vector2 v2)\n",
+                '[<DllImport("compiledVectors.exe")>]\n',
+                'extern double distanceTo2D(Vector2 v,Vector2 v2)\n',
 
-                     '[<DllImport("compiledVector.exe", CallingConvention = CallingConvention.StdCall)>]\n',
-                     "extern void vectorMovement(Vector2 v,double plusx, double plusy)\n",
+                '[<DllImport("compiledVectors.exe")>]\n',
+                'extern void vectorMovement2D(Vector2 v,double plusx, double plusy)\n',
 
-                     '[<DllImport("compiledVector.exe", CallingConvention = CallingConvention.StdCall)>]\n',
-                     "extern Vector2 midpoint(Vector2 v,Vector2 v2)\n",
+                '[<DllImport("compiledVectors.exe")>]\n',
+                'extern Vector2 midpoint2D(Vector2 v,Vector2 v2)\n',
 
-                     '[<DllImport("compiledVector.exe", CallingConvention = CallingConvention.StdCall)>]\n',
-                     "extern double percentDistance(Vector2 pos1, Vector2 pos2, double percent)\n",
-
-                     'printfn("test 2 passed")\n']
+                '[<DllImport("compiledVectors.exe")>]\n',
+                'extern double percentDistance2D(Vector2 pos1, Vector2 pos2, double percent)\n']
         for eachimports in imports2D:
             Header2D.write(eachimports)
             Header2D.flush()
         Header3D = open(args.path+"FSHeader3D.fs", "w")
         imports3D = ["module vector3\n",
-                     'open System.Runtime.InteropServices\n',
+                'open System.Runtime.InteropServices\n',
 
-                     '[<StructLayout(LayoutKind.Sequential)>]\n',
-                     'type Vector3 = val mutable X: double; val mutable Y:double; val mutable Z: double new(x, y, z) = { X = x; Y = y; Z=z }\n',
+                '[<StructLayout(LayoutKind.Sequential)>]\n',
+                'type Vector3 = val mutable X: double; val mutable Y:double; val mutable Z: double new(x, y, z) = { X = x; Y = y; Z=z }\n',
 
-                     '[<DllImport("compiledVector")>]\n',
-                     'extern Vector3 CreateVector3D(double x, double y, double z)\n',
+                '[<DllImport("compiledVectors.exe")>]\n',
+                'extern Vector3 CreateVector3D(double x, double y, double z)\n',
 
-                     '[<DllImport("compiledVector")>]\n',
-                     'extern double distanceTo3D(Vector3 v, Vector3 v2)\n',
+                '[<DllImport("compiledVectors.exe")>]\n',
+                'extern double distanceTo3D(Vector3 v, Vector3 v2)\n',
 
-                     '[<DllImport("compiledVector")>]\n',
-                     'extern void vectorMovement3D(Vector3 v, double plusx, double plusy, double plusz)\n',
+                '[<DllImport("compiledVectors.exe")>]\n',
+                'extern void vectorMovement3D(Vector3 v, double plusx, double plusy, double plusz)\n',
 
-                     '[<DllImport("compiledVector")>]\n',
-                     'extern Vector3 midpoint3D(Vector3 v, Vector3 v2)\n',
+                '[<DllImport("compiledVectors.exe")>]\n',
+                'extern Vector3 midpoint3D(Vector3 v, Vector3 v2)\n',
 
-                     '[<DllImport("compiledVector")>]\n',
-                     'extern double percentDistance3D(Vector3 pos1, Vector3 pos2, double percent)\n']
+                '[<DllImport("compiledVectors.exe")>]\n',
+                'extern double percentDistance3D(Vector3 pos1, Vector3 pos2, double percent)\n']
         for eachimports in imports3D:
             Header3D.write(eachimports)
         Header3D.flush()
@@ -176,17 +173,17 @@ if os.path.exists(fsproj):
             if "<ItemGroup>" in line and not any("<Compile Include=\"FSHeader2D.fs\"/>" in line for line in lines):
                 index = lines.index("  <ItemGroup>\n")
                 lines.insert(
-                    index+1, '    <Compile Include="FSHeader2D.fs"/>\n')
+                   index+1, '    <Compile Include="FSHeader2D.fs"/>\n')
 
             if "<ItemGroup>" in line and not any("<Compile Include=\"FSHeader3D.fs\"/>" in line for line in lines):
                 index = lines.index("  <ItemGroup>\n")
                 lines.insert(
-                    index+1, '    <Compile Include="FSHeader3D.fs"/>\n')
+                   index+1, '    <Compile Include="FSHeader3D.fs"/>\n')
 
             if any("<Compile Include=\"FSHeader2D.fs\"/>" in line for line in lines) and any("<Compile Include=\"FSHeader3D.fs\"/>" in line for line in lines):
                 with open(fsproj, "w") as out_file:
-                    out_file.writelines(lines)
-                    out_file.flush()
+                   out_file.writelines(lines)
+                   out_file.flush()
                 break
 
 
