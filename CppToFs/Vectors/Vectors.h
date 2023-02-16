fea8fecf9c9fdb2dac8e7e75a0de3a5e5dcd94e3
aurelienfernandez <aurelien.fernandez@algosup.com>
2023-02-16 10:02:54 +0100

    added the 's' in vectors.h

---
+++ CppToFs/Vectors/Vectors.h
@@ -0,0 +1,67 @@
+#include <math.h>
+#include <iostream>
+
+using namespace std;
+
+extern "C" class Vector2 {
+public:
+    double x;
+    double y;
+
+    Vector2(double x, double y) : x(x), y(y) {}
+};
+
+extern "C" class Vector3 {
+public:
+    double x;
+    double y;
+    double z;
+
+    Vector3(double x, double y, double z) : x(x), y(y), z(z) {}
+};
+
+extern "C" Vector2 *CreateVector2D(double x, double y)
+{
+   return new Vector2(x, y);
+}
+extern "C" double distanceTo2D(Vector2* pos1, Vector2* pos2)
+{   
+   return pos1->distanceTo(*pos2);
+}
+extern "C" void vectorMovement2D(Vector2* vector, double plusx, double plusy)
+{
+   vector->vectorMovement(plusx, plusy);
+   return;
+}
+extern "C" Vector2 *midpoint2D(Vector2* pos1, Vector2* pos2)
+{
+   return new Vector2(pos1->midpoint(*pos2));
+}
+extern "C" double percentDistance2D(Vector2* pos, Vector2* pos2, double percentOfDistance)
+{
+   return pos->percentDistance(*pos2, percentOfDistance);
+}
+
+
+
+extern "C" Vector3 *CreateVector3D(double x, double y,double z)
+{
+   return new Vector3(x, y, z);
+}
+extern "C" double distanceTo3D(Vector3* pos1, Vector3* pos2)
+{   
+   return pos1->distanceTo(*pos2);
+}
+extern "C" void vectorMovement3D(Vector3* vector, double plusx, double plusy, double plusz)
+{
+   vector->vectorMovement(plusx, plusy, plusz);
+   return;
+}
+extern "C" Vector3 *midpoint3D(Vector3* pos1, Vector3* pos2)
+{
+   return new Vector3(pos1->midpoint(*pos2));
+}
+extern "C" double percentDistance3D(Vector3* pos, Vector3* pos2, double percentOfDistance)
+{
+   return pos->percentDistance(*pos2, percentOfDistance);
+}
